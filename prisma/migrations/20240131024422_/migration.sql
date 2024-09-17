5788d513fe1439d4f38c9e5d9f9f5d5a0ea0507b
Ares <89419093+arestosora@users.noreply.github.com>
2024-09-17 08:23:24 -0500

    removed unused models

--- prisma/migrations/20240131024422_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `termsAccepted` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "termsAccepted",
-ADD COLUMN     "acceptTerms" BOOLEAN DEFAULT true;
